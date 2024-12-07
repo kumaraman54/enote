49a3e70008e0a9833ea9581f2e9821727a2de5ef
Aman Kumar Routh <routhaman831@gmail.com>
2024-12-07 21:22:23 +0530

    WORKING CORRECTLY

--- Backend/db.js
+++ Backend/db.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const mongoose = require("mongoose");
 const mongoURI = process.env.MONGOURI;
-console.log(mongoURI)
 
 const connectToMongo = async () => {
   try {
--- Backend/index.js
+++ Backend/index.js
@@ -8,13 +8,6 @@ const app = express();
 app.use(cors());
 
 connectToMongo();
-// const corsOptions = {
-//   origin: ['https://enote-qawk.vercel.app','http://localhost:5000'], // Allow only requests from this origin
-//   methods: 'GET,POST,PUT,OPTIONS,DELETE,UPDATE,PATCH', // Allow only these methods
-//   // credentials: true, // Allow credentials (cookies, authorization headers)
-//   //allowedHeaders: ['Content-Type', 'Authorization'], // Allow only these headers
-// };
-
 
 app.use(express.json()); // Middleware to parse JSON
 
