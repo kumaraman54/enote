dc4843b4a73cf2a549f8c762799494c364632dd2
Aman Kumar Routh <routhaman831@gmail.com>
2024-12-12 03:16:43 +0530

    updated backend url

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -15,7 +15,7 @@ const Signup = (props) => {
     const { name, email, password } = credentials;
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/createuser", {
+      const response = await fetch("https://enote-pi.vercel.app/api/auth/createuser", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
