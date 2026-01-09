ef630b07d2ca84ccbb0527a1ec7297cbe306b3ea
Aman Kumar Routh <routhaman831@gmail.com>
2026-01-09 21:08:24 +0530

    Update FE

--- enote-frontend/src/main.jsx
+++ enote-frontend/src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
+import React, { useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import './index.css'
+import App from './App'
+import Home from './components/Home'
+import About from './components/About'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import NoteState from './context/notes/NoteState'
+
+const Root = () => {
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+    setAlert({ msg: message, type })
+    setTimeout(() => setAlert(null), 1500)
+  }
+
+  const router = createHashRouter([
+    {
+      path: '/',
+      element: <App alert={alert} />,
+      children: [
+        { path: '/', element: <Home showAlert={showAlert} /> },
+        { path: 'about', element: <About /> },
+        { path: 'signup', element: <Signup showAlert={showAlert} /> },
+        { path: 'login', element: <Login showAlert={showAlert} /> },
+      ],
+    },
+  ])
+
+  return (
+    <React.StrictMode>
+      <NoteState>
+        <RouterProvider router={router} />
+      </NoteState>
+    </React.StrictMode>
+  )
+}
+
+createRoot(document.getElementById('root')).render(<Root />)
